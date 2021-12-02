9aa55a08ae9bce6b490b7c0209017324d4c14d9a
Martin Carrion <martincarrion2864212@gmail.com>
2021-12-02 15:37:43 -0300

    fix added

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors());
+app.use(express.static("build"));
 app.use(express.json());
 
 morgan.token("requestParams", (req) => JSON.stringify(req.body));
